e2118ed98318ecc96cc2fb778914c2c7724b897d
tsiswaryalakshmi <99864357+tsiswaryalakshmi@users.noreply.github.com>
2022-06-28 12:39:02 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,26 +2,26 @@ terraform {
   backend "azurerm" {
     resource_group_name  = "TerraformDemo"
     storage_account_name = "terraformcodeop"
-    container_name       = "tfstatefile"
-    key                  = "tfstatefile.tfstate"
+    container_name       = "tfstate"
+  #  key                  = "terraformgithubexample.tfstate"
   }
 }
- 
+
 provider "azurerm" {
   # The "feature" block is required for AzureRM provider 2.x.
   # If you're using version 1.x, the "features" block is not allowed.
   version = "~>2.0"
   features {}
 }
- 
+
 data "azurerm_client_config" "current" {}
- 
+
 #Create Resource Group
 resource "azurerm_resource_group" "tamops" {
   name     = "tamops"
   location = "eastus2"
 }
- 
+
 #Create Virtual Network
 resource "azurerm_virtual_network" "vnet" {
   name                = "tamops-vnet"
@@ -29,7 +29,7 @@ resource "azurerm_virtual_network" "vnet" {
   location            = "eastus2"
   resource_group_name = azurerm_resource_group.tamops.name
 }
- 
+
 # Create Subnet
 resource "azurerm_subnet" "subnet" {
   name                 = "subnet"
@@ -37,3 +37,4 @@ resource "azurerm_subnet" "subnet" {
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefix       = "192.168.0.0/24"
 }
+  
